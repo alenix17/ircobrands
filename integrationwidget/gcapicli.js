b1b9ce8af6bd37d43a6ee6bb1df5e319be6bac0f
alenix17 <136587057+alenix17@users.noreply.github.com>
2025-11-20 10:24:08 +0100

    Implement PKCE login method in gcapicli.js

--- integrationwidget/gcapicli.js
+++ integrationwidget/gcapicli.js
@@ -1,1 +1,17 @@
+const client = platformClient.ApiClient.instance;
+const client = platformClient.ApiClient.instance;
+client.setEnvironment(platformClient.PureCloudRegionHosts.eu_central_1);
 
+var clientId = "";
+var redirectUri = "";
+
+// Method1: Let loginPKCEGrant generate the code verifier
+client.loginPKCEGrant(clientId, redirectUri, { state: state })
+  .then((data) => {
+    console.log(data);
+    // Do authenticated things
+  })
+  .catch((err) => {
+    // Handle failure response
+    console.log(err);
+  });
